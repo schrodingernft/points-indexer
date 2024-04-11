67dbae4d11fd46c583cf95a3a222afba74a585cb
house1313 <house@schrodingernft.ai>
2024-04-11 22:59:09 +0800

    feat: fix sorting

--- src/Points.Indexer.Plugin/GraphQL/QueryPointsSum.cs
+++ src/Points.Indexer.Plugin/GraphQL/QueryPointsSum.cs
@@ -12,7 +12,7 @@ namespace Points.Indexer.Plugin.GraphQL;
 
 public partial class Query
 {
-    private const string SortScriptSourceFormat = "doc['{0}'].value as Integer";
+    private const string SortScriptSourceFormat = "if(params._source.{0} == null) return 0;else return Long.parseLong(params._source.{1})";
 
     [Name("getRankingList")]
     public static async Task<PointsSumListDto> GetRankingList(
@@ -39,18 +39,15 @@ public partial class Query
             f.Bool(b => b.Must(mustQuery));
         
         var sortType = input.Sorting == "DESC" ? SortOrder.Descending : SortOrder.Ascending;
-        // var recordList = await repository.GetListAsync(Filter, sortType: sortType,
-        //     sortExp: GetSortBy(input.SortingKeyWord), skip: input.SkipCount, limit: input.MaxResultCount);
-        //
         var sortField = input.SortingKeyWord switch
         {   
-            SortingKeywordType.FirstSymbolAmount => "FirstSymbolAmount",
-            SortingKeywordType.SecondSymbolAmount => "SecondSymbolAmount",
-            SortingKeywordType.FiveSymbolAmount => "FiveSymbolAmount",
+            SortingKeywordType.FirstSymbolAmount => "firstSymbolAmount",
+            SortingKeywordType.SecondSymbolAmount => "secondSymbolAmount",
+            SortingKeywordType.FiveSymbolAmount => "fiveSymbolAmount",
             _ => "FirstSymbolAmount"
         };
-        var sortScriptSource = String.Format(SortScriptSourceFormat, sortField);
-        
+        var sortScriptSource = String.Format(SortScriptSourceFormat, sortField, sortField);
+       
         IPromise<IList<ISort>> Sort(SortDescriptor<AddressPointsSumBySymbolIndex> s) =>
             s.Script(script => script.Type("number")
                 .Script(scriptDescriptor => scriptDescriptor.Source(sortScriptSource))
@@ -98,8 +95,23 @@ public partial class Query
         QueryContainer Filter(QueryContainerDescriptor<AddressPointsSumBySymbolIndex> f) => f.Bool(b => b.Must(mustQuery));
 
         var sortType = input.Sorting == "DESC" ? SortOrder.Descending : SortOrder.Ascending;
-        var recordList = await repository.GetListAsync(Filter, sortType: sortType,
-            sortExp: GetSortBy(input.SortingKeyWord), skip: input.SkipCount, limit: input.MaxResultCount);
+        var sortField = input.SortingKeyWord switch
+        {   
+            SortingKeywordType.FirstSymbolAmount => "firstSymbolAmount",
+            SortingKeywordType.SecondSymbolAmount => "secondSymbolAmount",
+            SortingKeywordType.FiveSymbolAmount => "fiveSymbolAmount",
+            _ => "FirstSymbolAmount"
+        };
+        var sortScriptSource = String.Format(SortScriptSourceFormat, sortField, sortField);
+        logger.LogDebug("sortScriptSource: {}", sortScriptSource);
+        
+        IPromise<IList<ISort>> Sort(SortDescriptor<AddressPointsSumBySymbolIndex> s) =>
+            s.Script(script => script.Type("number")
+                .Script(scriptDescriptor => scriptDescriptor.Source(sortScriptSource))
+                .Order(sortType));
+        
+        var recordList = await repository.GetSortListAsync(Filter, sortFunc: Sort, skip: input.SkipCount, limit: input.MaxResultCount);
+
         
         var dataList = objectMapper.Map<List<AddressPointsSumBySymbolIndex>, List<PointsSumDto>>(recordList.Item2);
         return new PointsSumListDto
